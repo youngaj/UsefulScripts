39f52d954294d923486d4b68b625f7feeb7bf975
Andre Young <Dreyoung79@yahoo.com>
2019-05-13 23:48:30 -0400

    Adds default printers to standard intern profile

--- boxstarter/interns/intern_standard.ps1
+++ boxstarter/interns/intern_standard.ps1
@@ -26,6 +26,7 @@ executeScript "FileExplorerSettings.ps1";
 executeScript "CommonDevTools.ps1";
 executeScript "Docker.ps1";
 executeScript "Browsers.ps1";
+executeScript "Printers.ps1";
 
 #--- Tools ---
 code --install-extension msjsdiag.debugger-for-chrome
---
+++ boxstarter/interns/scripts/Printers.ps1
@@ -0,0 +1,4 @@
+Add-Printer -ConnectionName \\hpgsprtp01\GS_12_E008_1
+Add-Printer -ConnectionName \\hpgsprtp02\B12-RE008-LJ 5200 
+Add-Printer -ConnectionName \\hpgsprtp01\GS_25_S086_1 
+Add-Printer -ConnectionName \\hpgsprtp01\GS_23_W114_1
